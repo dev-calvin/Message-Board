da3c8ac9d07da0788528198581041de1980c0edc
Calvin McClure <session1@START-Coding-MBP-16.local>
2017-03-06 13:12:54 -0700

    3/3/17

--- back/server.js
+++ back/server.js
@@ -15,6 +15,13 @@ var port = 8080;
 
 app.use(bodyParser.json());
 
+// take care of CORE's errors
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
+});
+
 //impliment an endpoint for message postings
 app.post('/api/message', function(req, res){
     console.log(req.body);
--- front/src/app/main/main.controller.js
+++ front/src/app/main/main.controller.js
@@ -3,16 +3,19 @@
     Mar 2 2017
 
     purpose: controller for main.html
-    
+
+    3/3/17 implimented $http service for postMessage()
 */
 
 export class MainController {
-  constructor ($log) {
+  constructor ($http) {
       'ngInject';
-      this.$log = $log;
+      this.$http = $http;
   }
 
   postMessage (){
-      this.$log.log('post');
+      this.$http.post('http://localhost:8080/api/message', {
+          msg: 'hello'
+      });
   }
 }
